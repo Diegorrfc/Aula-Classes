efb6b11a85f85c249aef8a5c5004276821f8a149
Diego <diego.rrfc@gmail.com>
2018-02-24 02:11:52 -0300

    Adicionar arquivos de projeto.

---
+++ Aula-Classes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            carro objeto = new carro("verde");
+            Console.WriteLine(objeto.Cor);
+            Console.ReadKey();
+        }
+    }
+
+    class carro
+    {
+        private string _cor;
+
+
+        public carro(String cor)
+        {
+            this._cor = cor;
+        }
+        
+        //public string Cor
+        //{
+        //    get { return _cor; }
+        //    set { _cor = value; }
+        //}
+
+        public string exemplo { get; set; }
+        public string Cor {
+
+            get { return _cor; }
+            set
+            {
+
+                if (value.Contains("Preto"))
+                {
+                    _cor = value;
+                }else
+                {
+                    throw new Exception("cores n suportada");
+                }
+            }
+
+        }
+
+        public string Descricao()
+        {
+             return "o carro eh:" + Cor;
+        }
+    }
+}
